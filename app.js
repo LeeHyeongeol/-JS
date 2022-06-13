492d2423e8ccf7c1b188c02c09c237ea2f8fc25c
hyeongirlife <leehyeongul@gmail.com>
2022-06-14 01:02:05 +0900

    [Common]_feat_form 태그 자동 렌더링 막는 기능

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector("input");
-const loginButton = document.querySelector("button");
 
-function ConditionButtonClick() {
-  if (loginInput.value.length <= 15) {
-    handleButtonClick()
-  } else if (loginInput.value.length > 15) {
-    alert("please write your username down shortly")
-  } else {
-    alert("please write your username right now!!!")
-  }
-  loginInput.value = ""
+function onLoginSubmit(event) {
+  //브라우저의 기본 동작을 막는다.
+  event.preventDefault()
+  console.log(event.value)
 }
 
 function handleButtonClick() {
@@ -19,5 +13,5 @@ function handleButtonClick() {
   loginInput.value = ""
 
 }
-
-loginButton.addEventListener("click", ConditionButtonClick)
+// onLoginSubmit() 이 있다면 submit event 발생 시 바로 함수를 실행시킨다.
+loginForm.addEventListener("submit", onLoginSubmit)
